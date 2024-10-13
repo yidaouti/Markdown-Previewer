e8f8a6a6088e07f9982851dbed7dee13cbe93b9c
yidaouti <chakchabusiness@gmail.com>
2024-10-14 00:34:08 +0100

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Markdown-Previewer/',
-})
+  build: {
+    outDir: 'dist', // Make sure this matches the directory used in your deploy script
+  },
+  base: '/Markdown-Previewer/' // Set this to your GitHub repository name
+});
